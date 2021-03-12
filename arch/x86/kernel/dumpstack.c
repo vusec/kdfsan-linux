36f66b61adf234217475a37bf4efaa82ddc3c54f
Jakob Koschel <jakobkoschel@aim.com>
2021-03-12 12:02:20 +0100

    llvm: Only print reliable addresses in printk_stack_address

--- arch/x86/kernel/dumpstack.c
+++ arch/x86/kernel/dumpstack.c
@@ -69,7 +69,8 @@ static void printk_stack_address(unsigned long address, int reliable,
 				 const char *log_lvl)
 {
 	touch_nmi_watchdog();
-	printk("%s %s%pB\n", log_lvl, reliable ? "" : "? ", (void *)address);
+  if (reliable)
+    printk("%s %s%pB\n", log_lvl, reliable ? "" : "? ", (void *)address);
 }
 
 static int copy_code(struct pt_regs *regs, u8 *buf, unsigned long src,
