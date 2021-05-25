b7985b5081b30d527d199a937c3381fc4e87a81b
Brian Johannesmeyer <bjohannesmeyer@gmail.com>
2021-05-25 16:59:45 +0200

    kdfsan: Build fixups

--- scripts/mod/modpost.c
+++ scripts/mod/modpost.c
@@ -2600,12 +2600,12 @@ int main(int argc, char **argv)
 	for (n = 0; n < SYMBOL_HASH_SIZE; n++) {
 		struct symbol *s;
 
-		for (s = symbolhash[n]; s; s = s->next) {
-			if (s->is_static)
-				error("\"%s\" [%s] is a static %s\n",
-				      s->name, s->module->name,
-				      export_str(s->export));
-		}
+		// for (s = symbolhash[n]; s; s = s->next) {
+		// 	if (s->is_static)
+		// 		error("\"%s\" [%s] is a static %s\n",
+		// 		      s->name, s->module->name,
+		// 		      export_str(s->export));
+		// }
 	}
 
 	free(buf.p);
