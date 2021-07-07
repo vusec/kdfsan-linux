2cf3f1c47a70809fcefe99d884ac422a311b4f6b
Brian Johannesmeyer <bjohannesmeyer@gmail.com>
2021-07-07 12:34:14 +0200

    Adds noinline to kdfsan interfaces

--- mm/kdfsan/kdfsan_interface.c
+++ mm/kdfsan/kdfsan_interface.c
@@ -18,7 +18,7 @@ void unset_rt(void) { kdf_is_in_rt = false; }
 
 /**** Interfaces inserted by pass ****/
 
-dfsan_label __dfsan_read_label(const void *addr, uptr size) {
+dfsan_label noinline __dfsan_read_label(const void *addr, uptr size) {
   if (size == 0) return 0;
   ENTER_RT(0);
   dfsan_label ret = kdf_read_label(addr,size);
@@ -27,7 +27,7 @@ dfsan_label __dfsan_read_label(const void *addr, uptr size) {
   return ret;
 }
 
-void __dfsan_set_label(dfsan_label label, void *addr, uptr size) {
+void noinline __dfsan_set_label(dfsan_label label, void *addr, uptr size) {
   if(size == 0) return;
   ENTER_RT();
   KDF_CHECK_LABEL(label);
@@ -35,7 +35,7 @@ void __dfsan_set_label(dfsan_label label, void *addr, uptr size) {
   LEAVE_RT();
 }
 
-dfsan_label __dfsan_union(dfsan_label l1, dfsan_label l2) {
+dfsan_label noinline __dfsan_union(dfsan_label l1, dfsan_label l2) {
   if (l1 == 0) return l2;
   if (l2 == 0) return l1;
   if (l1 == l2) return l1;
@@ -47,7 +47,7 @@ dfsan_label __dfsan_union(dfsan_label l1, dfsan_label l2) {
   return ret;
 }
 
-void __dfsan_vararg_wrapper(const char *fname) {
+void noinline __dfsan_vararg_wrapper(const char *fname) {
   ENTER_RT();
   printk("KDFSan ERROR: unsupported indirect call to vararg\n");
   LEAVE_RT();
@@ -55,7 +55,7 @@ void __dfsan_vararg_wrapper(const char *fname) {
 
 /**** Callback interfaces inserted by pass ****/
 
-dfsan_label __dfsan_load_callback(void *addr, uptr size, dfsan_label data_label, dfsan_label ptr_label) {
+dfsan_label noinline __dfsan_load_callback(void *addr, uptr size, dfsan_label data_label, dfsan_label ptr_label) {
   kdfinit_access_taint_sink(addr, size, _RET_IP_, data_label, ptr_label, false);
   ENTER_RT(data_label);
   KDF_CHECK_LABEL(data_label); KDF_CHECK_LABEL(ptr_label);
@@ -65,23 +65,23 @@ dfsan_label __dfsan_load_callback(void *addr, uptr size, dfsan_label data_label,
   return ret_label;
 }
 
-dfsan_label __dfsan_store_callback(void *addr, uptr size, dfsan_label data_label, dfsan_label ptr_label) {
+dfsan_label noinline __dfsan_store_callback(void *addr, uptr size, dfsan_label data_label, dfsan_label ptr_label) {
   kdfinit_access_taint_sink(addr, size, _RET_IP_, data_label, ptr_label, true);
   return data_label;
 }
 
-void __dfsan_mem_transfer_callback(void *dest, const void *src, uptr size) {
+void noinline __dfsan_mem_transfer_callback(void *dest, const void *src, uptr size) {
   if(size == 0) return;
   ENTER_RT();
   kdf_memtransfer(dest, src, size);
   LEAVE_RT();
 }
 
-void __dfsan_cmp_callback(dfsan_label combined_label) { }
+void __dfsan_cmp_callback(dfsan_label combined_label) { /* Add noinline function attribute if/when this is eventually used */ }
 
 /**** Interfaces not inserted by pass ****/
 
-void dfsan_add_label(dfsan_label label_src, void *addr, uptr size) {
+void noinline dfsan_add_label(dfsan_label label_src, void *addr, uptr size) {
   if(size == 0 || label_src == 0) return;
   ENTER_RT();
   KDF_CHECK_LABEL(label_src);
@@ -90,7 +90,7 @@ void dfsan_add_label(dfsan_label label_src, void *addr, uptr size) {
 }
 
 // TODO: userdata unused; remove
-dfsan_label dfsan_create_label(const char *desc, void *userdata) {
+dfsan_label noinline dfsan_create_label(const char *desc, void *userdata) {
   ENTER_RT(0);
   dfsan_label ret = kdf_create_label(desc);
   KDF_CHECK_LABEL(ret);
@@ -98,7 +98,7 @@ dfsan_label dfsan_create_label(const char *desc, void *userdata) {
   return ret;
 }
 
-int dfsan_has_label(dfsan_label label, dfsan_label elem) {
+int noinline dfsan_has_label(dfsan_label label, dfsan_label elem) {
   if (label == elem) return true;
   ENTER_RT(false);
   KDF_CHECK_LABEL(label); KDF_CHECK_LABEL(elem);
@@ -107,7 +107,7 @@ int dfsan_has_label(dfsan_label label, dfsan_label elem) {
   return ret;
 }
 
-dfsan_label dfsan_has_label_with_desc(dfsan_label label, const char *desc) {
+dfsan_label noinline dfsan_has_label_with_desc(dfsan_label label, const char *desc) {
   ENTER_RT(0);
   KDF_CHECK_LABEL(label);
   dfsan_label ret = kdf_has_label_with_desc(label,desc);
@@ -116,37 +116,37 @@ dfsan_label dfsan_has_label_with_desc(dfsan_label label, const char *desc) {
   return ret;
 }
 
-dfsan_label dfsan_get_label_count(void) {
+dfsan_label noinline dfsan_get_label_count(void) {
   ENTER_RT(0);
   dfsan_label ret = kdf_get_label_count();
   LEAVE_RT();
   return ret;
 }
 
-dfsan_label dfsan_read_label(const void *addr, uptr size) {
+dfsan_label noinline dfsan_read_label(const void *addr, uptr size) {
   return __dfsan_read_label(addr, size);
 }
 
-void dfsan_set_label(dfsan_label label, void *addr, uptr size) {
+void noinline dfsan_set_label(dfsan_label label, void *addr, uptr size) {
   __dfsan_set_label(label, addr, size);
 }
 
-dfsan_label dfsan_union(dfsan_label l1, dfsan_label l2) {
+dfsan_label noinline dfsan_union(dfsan_label l1, dfsan_label l2) {
   return __dfsan_union(l1,l2);
 }
 
-dfsan_label __dfsw_dfsan_get_label(long data, dfsan_label data_label, dfsan_label *ret_label) {
+dfsan_label noinline __dfsw_dfsan_get_label(long data, dfsan_label data_label, dfsan_label *ret_label) {
   *ret_label = 0;
   return data_label;
 }
 
-void dfsan_mem_transfer_callback(void *dest, const void *src, uptr size) {
+void noinline dfsan_mem_transfer_callback(void *dest, const void *src, uptr size) {
   __dfsan_mem_transfer_callback(dest, src, size);
 }
 
 /**** Misc. interfaces ****/
 
-void dfsan_copy_label_info(dfsan_label label, char * dest, size_t count) {
+void noinline dfsan_copy_label_info(dfsan_label label, char * dest, size_t count) {
   ENTER_RT();
   KDF_CHECK_LABEL(label);
   kdf_copy_label_info(label, dest, count);
